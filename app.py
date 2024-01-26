affd7d8a8836736b964cf9550a4d14fb70a129c5
aimdatascintist <ganeshdatascince11@gmail.com>
2024-01-26 22:17:44 +0530

    add model folder and trigger runner

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import tensorflow as tf
 import numpy as np
 
 # Load your custom model for COVID, NORMAL, PNEUMONIA classification
-model = tf.keras.models.load_model(r"C:\Users\Harshali\ds\Disease Recognition\artifacts\training/model.h5")
 
+#model = tf.keras.models.load_model(r"C:\Users\Harshali\ds\Disease Recognition\artifacts\training/model.h5")
+
+ model = tf.keras.models.load_model(r"C:\Users\Harshali\Downloads\Disease_Recognition\model\model.h5")
 # Set page title and icon
 st.set_page_config(page_title="Disease Recognition (Chest X-ray Classifier)", page_icon="🩺")
 
